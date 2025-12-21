3a4c21f35823a6b3ae88dd1f0ca40e7b49e6e68f
pablo <vera.pabloleonel@gmail.com>
2025-12-21 00:36:32 -0300

    insertar html en div

--- 2-script.js
+++ 2-script.js
@@ -3,6 +3,7 @@ const sectionMenu = document.getElementById("menu")
 const sectionConsulta = document.getElementById("consulta")
 const FormUsuario = document.getElementById("FormUsuario");
 const FormContraseña = document.getElementById("FormContraseña");
+const contenMenu = document.getElementById("contenMenu")
 
 
 /* hace en un futuro una version que use return envez de variables globales */
@@ -36,7 +37,10 @@ function formularios () {
 
     contraseña = document.getElementById("contraseña").value;
     console.log(contraseña); // acá tenés el string
+
+    menu()
     })
+
 
 }
 
@@ -45,6 +49,18 @@ function validacion(){} /* este por ahora no existe */
 function menu(){
 
 sectionMenu.style.display = "flex"
+sectionBienvenida.style.display = "none"
+
+let opciones = 
+    `<h1>hola ${usuario} </h1>
+    <h2>que desea hacer?</h2>
+    <h3>1-Consultar saldo</h3>
+    <h3>2-Depositar saldo</h3>
+    <h3>3-Retirar saldo</h3>
+    <p>4-salir</p>`
+
+
+contenMenu.innerHTML = opciones
 
 
 }
